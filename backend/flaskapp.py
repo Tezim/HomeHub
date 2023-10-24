2a099fe5a737c799ad3814aac4a9e59b3c1932d1
Tatiana Hýlová <79145777+Tezim@users.noreply.github.com>
2023-10-24 20:35:57 +0200

    test

--- backend/flaskapp.py
+++ backend/flaskapp.py
@@ -1,8 +1,8 @@
 import os
 from flask import redirect, url_for, render_template, request, flash, send_file, session, make_response
 from flask_login import current_user, login_user, logout_user, login_required
-from app_config import app, db, login
-from DTO.user_model import User
+from backend.app_config import app, db, login
+from backend.DTO.user_model import User
 from threading import Thread, Event
 thread = Thread()
 thread_stop_event = Event()
@@ -19,7 +19,7 @@ with app.app_context():
 @app.route("/")
 def home():
     if current_user.is_authenticated:
-        return {}
+        return redirect('dashboard')
     else:
         return redirect('login')
 
@@ -31,7 +31,8 @@ def login():
         password = request.form["password"]
         u = User.query.filter_by(name=user).first()
         return {}
-
+    else:
+        return {}
 
 
 @app.route("/logout")
@@ -41,17 +42,22 @@ def logout():
     session.pop('id', None)
     return redirect(url_for('login'))
 
+@app.route("/dashboard")
+def dashboard():
+    return {"dahboard": "working"}
+
+
 @app.errorhandler(401)
 def unauthorized(e):
     return {}
+
 
 @app.route("/nope")
 def nope():
     return "I give up :("
 
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-
     app.debug = True
-    #app.run(host='0.0.0.0')
