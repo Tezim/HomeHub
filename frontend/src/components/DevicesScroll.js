303d56ed28ac9b3fc26697dcc959e2ed2d1c9fef
Viktor Valacsai <viktor.valacsai@gmail.com>
2023-11-15 21:35:15 +0100

    add devices page

--- frontend/src/components/DevicesScroll.js
+++ frontend/src/components/DevicesScroll.js
@@ -1,7 +1,21 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import { getDevicesForRoomId } from "../services/DevicesService";
+import "./scroll.css";
 
-const DevicesScroll = ({ appliances }) => {
+const DevicesScroll = ({ appliances, rooms, categories, onSelect }) => {
   const [isSelected, setIsSelected] = useState("");
+  const [isCategoryOpen, setIsCategoryOpen] = useState("");
+  const [isRoomsOpen, setIsRooms] = useState(true);
+  const [isRoomOpen, setIsRoom] = useState("");
+  const [isDevicesOpen, setIsDevicesOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
+  const [devicesForRoom, setDevicesForRoom] = useState([]);
+
+  const getDevicesForRoom = useCallback((id) => {
+    getDevicesForRoomId(id)
+      .then((response) => setDevicesForRoom(response.data))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <div
@@ -10,38 +24,244 @@ const DevicesScroll = ({ appliances }) => {
         flexDirection: "column",
         alignItems: "center",
         padding: "10px",
-        overflowY: "auto",
+        overflow: "auto",
         overflowX: "hidden",
-        maxHeight: "60vh",
+        maxHeight: "65vh",
       }}
+      className="scrollable-div"
     >
-      {appliances?.map((a) => {
-        return (
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              backgroundColor: isSelected === a.name ? "orange" : "#454544",
-              borderRadius: "20px",
-              padding: "10px",
-              marginBottom: "10px",
-              flex: 1,
-              minWidth: "25vw",
-            }}
-            onClick={() => setIsSelected(a.name)}
-          >
+      <div
+        style={{
+          display: "flex",
+          backgroundColor: "black",
+          alignItems: "center",
+          width: "100%",
+          justifyContent: "space-between",
+          borderRadius: "10px",
+          minWidth: "25vw",
+          marginBottom: "5px",
+          cursor: "pointer",
+        }}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div
+          style={{
+            fontSize: "25px",
+            textAlign: "center",
+            padding: "5px",
+          }}
+        >
+          Categories
+        </div>
+        <span style={{ paddingRight: "5px", fontSize: "25px" }}>
+          {isOpen ? "▲" : "▼"}
+        </span>
+      </div>
+      {isOpen && (
+        <div style={{ padding: "0px 10px 0px 10px", width: "100%" }}>
+          {categories?.map((c, i) => {
+            return (
+              <>
+                <div
+                  style={{
+                    display: "flex",
+                    backgroundColor: "#2b2b2b",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    width: "100%",
+                    padding: "5px 5px 5px 8px",
+                    fontSize: "20px",
+                    borderRadius: "10px",
+                    cursor: "pointer",
+                    margin: "5px 0px 5px 0px",
+                  }}
+                  key={i}
+                  onClick={() => {
+                    isCategoryOpen === c.name
+                      ? setIsCategoryOpen("")
+                      : setIsCategoryOpen(c.name);
+                  }}
+                >
+                  <div>{c.name}</div>
+                  <span style={{ paddingRight: "5px" }}>
+                    {isCategoryOpen !== c.name ? "▲" : "▼"}
+                  </span>
+                </div>
+                <div
+                  style={{ padding: isCategoryOpen === c.name ? "5px" : "0px" }}
+                >
+                  {isCategoryOpen === c.name &&
+                    c.devices?.map((d, i) => {
+                      return (
+                        <div
+                          style={{
+                            backgroundColor:
+                              isSelected === d.device_id ? "orange" : "#454544",
+                            width: "100%",
+                            borderRadius: "10px",
+                            padding: "5px",
+                            marginBottom: "5px",
+                            fontSize: "18px",
+                          }}
+                          key={i}
+                          onClick={() => {
+                            isSelected === d.device_id
+                              ? setIsSelected("")
+                              : setIsSelected(d.device_id);
+                            onSelect(d);
+                          }}
+                        >
+                          <div key={i}>{d.name}</div>
+                        </div>
+                      );
+                    })}
+                </div>
+              </>
+            );
+          })}
+        </div>
+      )}
+      <div
+        style={{
+          display: "flex",
+          backgroundColor: "black",
+          alignItems: "center",
+          width: "100%",
+          justifyContent: "space-between",
+          borderRadius: "10px",
+          minWidth: "25vw",
+          marginBottom: "5px",
+          cursor: "pointer",
+        }}
+        onClick={() => setIsRooms(!isRoomsOpen)}
+      >
+        <div
+          style={{
+            fontSize: "25px",
+            textAlign: "center",
+            padding: "5px",
+          }}
+        >
+          Rooms
+        </div>
+        <span style={{ paddingRight: "5px", fontSize: "25px" }}>
+          {isRoomsOpen ? "▲" : "▼"}
+        </span>
+      </div>
+      {isRoomsOpen && (
+        <div style={{ padding: "0px 10px 0px 10px", width: "100%" }}>
+          {rooms?.map((r, i) => {
+            return (
+              <>
+                <div
+                  style={{
+                    display: "flex",
+                    backgroundColor: "#2b2b2b",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    width: "100%",
+                    padding: "5px 5px 5px 8px",
+                    fontSize: "20px",
+                    borderRadius: "10px",
+                    cursor: "pointer",
+                    margin: "5px 0px 5px 0px",
+                  }}
+                  key={i}
+                  onClick={() => {
+                    getDevicesForRoom(r.room_id);
+                    isRoomOpen === r.name ? setIsRoom("") : setIsRoom(r.name);
+                  }}
+                >
+                  <div>{r.name}</div>
+                  <span style={{ paddingRight: "5px" }}>
+                    {isRoomOpen !== r.name ? "▲" : "▼"}
+                  </span>
+                </div>
+                <div style={{ padding: isRoomOpen === r.name ? "5px" : "0px" }}>
+                  {isRoomOpen === r.name &&
+                    devicesForRoom?.map((d, i) => {
+                      return (
+                        <div
+                          style={{
+                            backgroundColor:
+                              isSelected === d.device_id ? "orange" : "#454544",
+                            width: "100%",
+                            borderRadius: "10px",
+                            padding: "5px",
+                            marginBottom: "5px",
+                            fontSize: "18px",
+                          }}
+                          key={i}
+                          onClick={() => {
+                            isSelected === d.device_id
+                              ? setIsSelected("")
+                              : setIsSelected(d.device_id);
+                            onSelect(d);
+                          }}
+                        >
+                          <div>{d.name}</div>
+                        </div>
+                      );
+                    })}
+                </div>
+              </>
+            );
+          })}
+        </div>
+      )}
+      <div
+        style={{
+          display: "flex",
+          backgroundColor: "black",
+          width: "100%",
+          marginBottom: "5px",
+          borderRadius: "10px",
+          padding: "5px",
+          fontSize: "25px",
+          minWidth: "25vw",
+          justifyContent: "space-between",
+          alignItems: "center",
+          cursor: "pointer",
+        }}
+        onClick={() => setIsDevicesOpen(!isDevicesOpen)}
+      >
+        <div>All Devices</div>
+        <span>{isDevicesOpen ? "▲" : "▼"}</span>
+      </div>
+      {isDevicesOpen &&
+        appliances?.map((a, i) => {
+          return (
             <div
               style={{
-                fontSize: "25px",
-                color: "black",
+                display: "flex",
+                flexDirection: "column",
+                backgroundColor:
+                  isSelected === a.device_id ? "orange" : "#454544",
+                borderRadius: "20px",
+                padding: "10px",
+                marginBottom: "10px",
+                flex: 1,
+                minWidth: "25vw",
               }}
+              onClick={() => {
+                isSelected === a.device_id
+                  ? setIsSelected("")
+                  : setIsSelected(a.device_id);
+                onSelect(a);
+              }}
+              key={i}
             >
-              {a.name}
+              <div
+                style={{
+                  fontSize: "25px",
+                }}
+              >
+                {a.name}
+              </div>
+              <div style={{ fontSize: "15px", color: "#858483" }}>{a.room}</div>
             </div>
-            <div style={{ fontSize: "15px", color: "#858483" }}>{a.room}</div>
-          </div>
-        );
-      })}
+          );
+        })}
     </div>
   );
 };
--- frontend/src/pages/DevicesPage.js
+++ frontend/src/pages/DevicesPage.js
@@ -6,10 +6,16 @@ import { useEffect } from "react";
 import { isAuthenticated } from "../components/helpers/Helpers";
 import CustomLoading from "../components/custom/CustomLoading";
 import { getDevicesFromDb } from "../services/DevicesService";
+import { getCategoriesFromDb } from "../services/CategoriesService";
+import { getRoomsFromDb } from "../services/RoomsService";
+import SpecificationText from "../components/SpecificationText";
 
 const DevicesPage = () => {
   const [devices, setDevices] = useState([]);
+  const [rooms, setRooms] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [categories, setCategories] = useState([]);
+  const [selectedDevice, setSelectedDevice] = useState({});
   const history = useNavigate();
   const authenticated = isAuthenticated();
 
@@ -21,9 +27,40 @@ const DevicesPage = () => {
       .finally(() => setLoading(false));
   };
 
+  const getCategories = () => {
+    setLoading(true);
+    getCategoriesFromDb()
+      .then((response) => setCategories(response.data))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  };
+
+  const getRooms = () => {
+    setLoading(true);
+    getRoomsFromDb()
+      .then((response) => setRooms(response.data))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  };
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0"); // +1 because months are 0-indexed
+    const year = date.getFullYear();
+
+    const formattedDate = `${day}/${month}/${year}`;
+    return formattedDate;
+  };
+
   useEffect(() => {
     if (!authenticated) history("/");
-    getDevices();
+    let promises = [];
+    promises.push(getDevices());
+    promises.push(getCategories());
+    promises.push(getRooms());
+    Promise.all(promises);
   }, [authenticated, history]);
 
   if (loading) {
@@ -50,7 +87,59 @@ const DevicesPage = () => {
         }}
       />
       <div style={{ display: "flex", flexDirection: "row", padding: "10px" }}>
-        <DevicesScroll appliances={devices} />
+        <DevicesScroll
+          appliances={devices}
+          categories={categories}
+          rooms={rooms}
+          onSelect={(device) => setSelectedDevice(device)}
+        />
+        {Object.keys(selectedDevice).length > 0 && (
+          <div
+            style={{
+              backgroundColor: "#2b2b2b",
+              width: "70%",
+              margin: "5px",
+              borderRadius: "10px",
+              minHeight: "65vh",
+              padding: "20px",
+              fontFamily: "inherit",
+              fontSize: "25px",
+            }}
+          >
+            <div style={{ borderBottom: "1px solid orange", padding: "10px" }}>
+              Specifications
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                margin: "30px 20px 20px 20px",
+              }}
+            >
+              <SpecificationText spec={"Room"} value={selectedDevice.name} />
+              <SpecificationText
+                spec={"IP Address"}
+                value={selectedDevice.IP_address}
+              />
+              <SpecificationText
+                spec={"MAC Address"}
+                value={selectedDevice.MAC_address}
+              />
+              <SpecificationText
+                spec={"Additional Info"}
+                value={selectedDevice.add_info}
+              />
+              <SpecificationText
+                spec={"Last Modified"}
+                value={formatDate(selectedDevice.modified)}
+              />
+              <SpecificationText
+                spec={"Created"}
+                value={formatDate(selectedDevice.created)}
+              />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
